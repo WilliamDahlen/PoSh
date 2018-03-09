15f013c18cbf00c814214fd6766ec7e3d5fb8473
WilliamDahlen <36991168+WilliamDahlen@users.noreply.github.com>
2018-03-09 12:28:23 +0100

    Add files via upload

---
+++ CountADusersinOU.ps1
@@ -0,0 +1,11 @@
+﻿<#
+
+Title: Count users in specific OU
+Dato: 01.01.2017
+Auth: WAD
+
+#>
+
+$OU="OU=Users,DC=adatum,DC=com" #change this with your own string.
+
+(Get-ADUser -Filter * -SearchBase $OU).count
