690ebb4d00d863a1dd4c5df92cb3c298671f10db
WilliamDahlen <36991168+WilliamDahlen@users.noreply.github.com>
2018-05-08 10:35:55 +0200

    Simple script to get attributes from txt file
    Made a simple script to get spesific attributes from users in a txtfile. No need for delimiter, just make sure the source file has each username  on different lines.

---
+++ getAttributesFromUsersInTxt.ps1
@@ -0,0 +1,9 @@
+﻿$users = ForEach ($user in $(Get-Content sourceFile.txt)) {
+
+    
+    Get-AdUser $user -Server contoso.com -Properties displayName,cn
+}
+    
+ $users |
+ Select-Object displayName,cn |
+ Export-CSV -Path outFile.csv -NoTypeInformation
